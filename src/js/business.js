7d424c7118b8ff7de0e55f7e4901cedb3f2b74d1
Fabunmi Ebenezer <fabunmiebenezer37@gmail.com>
2024-09-19 19:02:05 +0100

    add the business logic for the memoty game

---
+++ src/js/business.js
@@ -0,0 +1,29 @@
+// MemoryGame class
+ export default class MemoryGame {
+    constructor() {
+      this.flippedCards = [];
+      this.matches = 0;
+    }
+    shuffleCards(cards) {
+      const cardArray = Array.from(cards);
+      cardArray.sort(() => Math.random()- 0.5);
+      return cardArray;
+    }
+    checkMatch(card1, card2) {
+      const img1 = card1.querySelector('.back img').src;
+      const img2 = card2.querySelector('.back img').src;
+      return img1 === img2
+    }
+    checkForMatch(card1, card2) {
+      if (this.checkMatch(card1, card2)) {
+        this.matches ++;
+        console.log('Match found!');
+        return true
+      } else{
+        return false
+      }
+    }
+    resetGame() {
+      this.matches = 0;
+    }
+  }
